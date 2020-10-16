d154dbdeb00e6974830814304b9cbc058586db92
morswin22 <xmorswinx@gmail.com>
2020-10-16 15:37:09 +0200

    :sparkles: Add multiple cameras support

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,9 +1,30 @@
-let capture;
+const captures = [];
+let captureID;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  capture = createCapture(VIDEO);
-  capture.hide();
+
+  navigator.mediaDevices.enumerateDevices().then(deviceInfos => {
+    let i = 0;
+    for (const info of deviceInfos) {
+      if (info.kind == 'videoinput') {
+        const li = document.createElement('li');
+        li.innerText = info.label;
+        li.setAttribute('data-id', i);
+        li.addEventListener('click', e => {
+          captureID = parseInt(e.target.getAttribute('data-id'));
+        });
+        capturesList.appendChild(li);
+        const capture = createCapture({
+          video: info
+        });
+        capture.hide();
+        captures.push(capture);
+        i++;
+      }
+    }
+    if (captures.length) captureID = 0;
+  });
 }
 
 function windowResized() {
@@ -11,6 +32,9 @@ function windowResized() {
 }
 
 function draw() {
+  if (captureID === undefined) return;
+  const capture = captures[captureID]
+
   background(229);
 
   let width, height;
