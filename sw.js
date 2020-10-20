d2efa04aadb823360d4a75209480a4e4b8bf669f
morswin22 <xmorswinx@gmail.com>
2020-10-20 21:00:56 +0200

    :sparkles: Add service worker for caching

---
+++ sw.js
@@ -0,0 +1,26 @@
+const versionTag = 'v1';
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request).then(response => {
+        return caches.open(versionTag).then(cache => {
+          if (event.request.url.indexOf('http') === 0) cache.put(event.request, response.clone());
+          return response;
+        });  
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map(key => {
+        if (versionTag !== key) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
